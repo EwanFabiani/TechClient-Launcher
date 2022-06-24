5a1820bb36189e6d1087acfdb4d2e0c4b140842e
EwanFabiani <ewan.fabiani.c@gmail.com>
2022-06-24 11:40:16 +0200

    Fixed Ram Bugs

--- src/windows/settings/script.js
+++ src/windows/settings/script.js
@@ -4,15 +4,17 @@ var slider = document.getElementById("ram");
 var output = document.getElementById("value");
 var warning = document.getElementById("warning");
 var warning1 = document.getElementById("warning1");
+const maxMemory = Number(os.totalmem() / (1000 * 1024 * 1024)).toFixed(1);
+slider.max = maxMemory;
 output.innerHTML = "2.0 GB";
-const maxMemory = Number(os.totalmem() / (1024 * 1024 * 1024)).toFixed(1);
 
 slider.oninput = function() {
-    if (this.value > (maxMemory/2)+0.4) {
+    if (this.value > (maxMemory/2)+0.1) {
         output.innerHTML = Number(this.value).toFixed(1) + " GB";
         output.style.color = "red";
         warning.style.display = "block";
         warning1.style.display = "block";
+        
     }else {
         output.innerHTML = Number(this.value).toFixed(1) + " GB";
         output.style.color = "black";
